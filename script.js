8eb969f45ca81839b11433b14bc4b3d4ef95f34c
Aryan Kondapalli <aryan.k2001@yahoo.com>
2022-09-09 17:08:18 -0500

    Add erase functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const griddiv = document.querySelector('.grid-container');
 const resize = document.querySelector('#resize-button');
+const erase = document.querySelector('#erase-button')
 let gridDimensions = 16;
 
 function removeChildren(parent){
@@ -11,6 +12,7 @@ function removeChildren(parent){
 function makeGrid(dimensions){
     for(let i = 0; i < dimensions**2; i++){
     const div = document.createElement('div');
+    div.classList.add('grid-div')
     let dim = (960 / dimensions) - 2;
     div.style.width = dim + 'px';
     div.style.height = dim + 'px';
@@ -23,12 +25,19 @@ function makeGrid(dimensions){
 
 let input = 0;
 resize.addEventListener('click', function() {
-    input = prompt('Please enter a value between 16 and 100:', 16);
+    do{
+        input = prompt('Please enter a value between 16 and 100:', 16);
+    } while(isNaN(input) || input > 100 || input < 16)
     removeChildren(griddiv);
     makeGrid(input);
 });
 
-
+erase.addEventListener('click', function(){
+    const grid = document.querySelectorAll('.grid-div')
+    grid.forEach((div) => {
+        div.style.backgroundColor = 'white';
+      });
+});
 
 
 makeGrid(16);
